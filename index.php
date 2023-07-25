1172ba544218bdf35e0e1d699c543dcbe3c6457c
leo99effert <leo99effert@outlook.com>
2023-07-25 10:00:02 +0200

    home page

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
 <?php include("includes/header.php"); ?>
 
+<?php
 
-        <div class="row">
+$photos = Photo::find_all();
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+?>
 
-    
-            
-          
-         
-
+<div class="row">
+    <div class="col-md-12">
+        <div class="thumbnails row">
+        <?php foreach ($photos as $photo): ?>
+            <div class="col-xs-6 col-md-3">
+                <a class="thumbnail" href="photo.php?id=<?php echo $photo->id ?>">
+                    <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                </a>
             </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
+        <?php endforeach; ?>
         </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+    </div>
+</div>
+<?php include("includes/footer.php"); ?>
